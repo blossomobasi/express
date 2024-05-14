6881ab374cb995d6d92d28ba2e7cbbd4a52d451f
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-14 02:47:54 +0100

    Add Built-in Middleware

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ const authentication = require("./authentication");
 
 const app = express();
 
+/*
+console.log(`NODE_ENV: ${process.env.NODDE_ENV}`);
+console.log(`app: ${app.get("env")}`);
+*/
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
-app.use(morgan("tiny"));
+
+// Enable only on development machine
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
+}
 
 app.use(logger);
 app.use(authentication);
